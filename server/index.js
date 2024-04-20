524860cbc7ada4cb155c0c67896d1f855d39e22c
heyvipul <vipulgirhestar@gmail.com>
2024-04-20 13:47:46 +0530

    user-left/bug-fix

--- server/index.js
+++ server/index.js
@@ -52,7 +52,11 @@ io.on("connection",(socket)=>{
     
     
     socket.on("disconnect",()=>{
-        console.log("user had left!");
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit("message",{user:'admin',text:`${user.name} has left.`})
+        }
     })
 })
 
